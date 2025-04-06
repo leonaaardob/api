5c0b31262f4ba1af31dbff62e8c35e8e3d660639
Luke Policinski <Luke@LukePOLO.com>
2025-04-06 14:13:31 -0400

    chore: add secure cookie

--- src/utilities/getCookieOptions.ts
+++ src/utilities/getCookieOptions.ts
@@ -34,7 +34,7 @@ export function getCookieOptions(
     httpOnly: true,
     domain: process.env.AUTH_COOKIE_DOMAIN || `.${process.env.WEB_DOMAIN}`,
     maxAge: 14 * 24 * 60 * 60 * 1000,
-    secure: true,
+    secure: process.env.SECURE_COOKIE || true,
   },
 ): CookieOptions {
   return Object.assign({}, defaultOptions, options);
