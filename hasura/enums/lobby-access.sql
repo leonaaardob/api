f8602aab740848ab2557d8b284ce419925987093
Luke Policinski <Luke@LukePOLO.com>
2025-04-06 20:01:26 -0400

    bug: fix missing enum for lobby access (#36)

--- hasura/enums/lobby-access.sql
+++ hasura/enums/lobby-access.sql
@@ -1,5 +1,6 @@
 insert into e_lobby_access ("value", "description") values
     ('Invite', 'Invite Only'),
     ('Open', 'Public'),
-    ('Friends', 'Friends Only')
+    ('Friends', 'Friends Only'),
+    ('Private', 'Private')
 on conflict(value) do update set "description" = EXCLUDED."description"
